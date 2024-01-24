479a8fd34cc7e67af254d1e1d7c8311e18269744
Pol Alvarez <pol.avms@gmail.com>
2024-01-24 11:10:59 +0100

    fixed: database/services.py

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, render_template, jsonify, request
 from database.db import init_db
+import openai
 
 load_dotenv()
 
-db_uri = os.getenv('SQLALCHEMY_APIS_DATABASE_URI')
-validate_user_prompt = os.getenv('VALIDATE_USER_PROMPT')
+db_uri = os.getenv("SQLALCHEMY_APIS_DATABASE_URI")
+validate_user_prompt = os.getenv("VALIDATE_USER_PROMPT")
 
 app = Flask(__name__)
 db = init_db(db_uri, app)
@@ -20,17 +21,18 @@ def get_completion(prompt, model="gpt-3.5-turbo", max_tokens=5000):
 
     return response.choices[0].message["content"]
 
+
 def llm_get_prompt_tasks(service, prompt):
     prompt = f"""
     Given a user prompt your must extract a list of tasks that need to interact with an API  to perform the task the user asked for.
 
 
-    Example 1:  
+    Example 1:
         Prompt: Give me the list of repositories in GitHub that I contributed and have more than 10 collaborators sorted by number of stars
 
         Answer:
         Get Authenticated user
-        List user repositories 
+        List user repositories
         Get repository collaborators
 
 
@@ -50,11 +52,12 @@ def llm_get_prompt_tasks(service, prompt):
     """
 
     tasks = get_completion(prompt, model="gpt-4-1106-preview")
-    return tasks.split('\n')
+    return tasks.split("\n")
+
 
 def llm_get_prompt_tags(service, user_prompt):
     prompt = f"""
-    Given the API for a service and a user prompot reply with the list of tags should be 
+    Given the API for a service and a user prompt reply with the list of tags should be
     used to find the relevant endpoints.
 
 
@@ -64,7 +67,7 @@ def llm_get_prompt_tags(service, user_prompt):
 
         Method: Get
         Tags: Get user
-    
+
     Example 2:
         Service: Github
         Task: List user repositories
@@ -72,7 +75,7 @@ def llm_get_prompt_tags(service, user_prompt):
         Method: Get
         Tags: GET repo details contributors
 
-    
+
     Service: {service}
     User prompt: {user_prompt}
     Tags:
@@ -80,22 +83,23 @@ def llm_get_prompt_tags(service, user_prompt):
 
     return get_completion(prompt)
 
+
 def llm_get_task_endpoints(service, task):
-    
     return []
 
 
 def llm_generate_script(service, token, user_prompt, endpoints):
     pass
 
+
 def process_user_prompt(service, token, prompt):
     try:
         # TODO: ask the llm if this request makes sense
-        
+
         split_in_tasks = llm_get_prompt_tasks(service, prompt)
         if len(tasks) == 0:
             return None, f"No task for the given prompt {prompt}"
-        
+
         endpoints = []
         for task in tasks:
             llm_get_endpoints = llm_get_task_endpoints(service, task)
@@ -104,7 +108,7 @@ def process_user_prompt(service, token, prompt):
         code = llm_generate_script(service, token, prompt, endpoints)
 
         return code, None
-       
+
     except Exception as e:
         print(e)
         return None, "Error processing user prompt"
@@ -112,47 +116,44 @@ def process_user_prompt(service, token, prompt):
     # Filter relevant endpoints from the database
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/gen-script', methods=['POST'])
+
+@app.route("/gen-script", methods=["POST"])
 def chat():
     data = request.get_json()
 
-    if 'service' not in data or data['service'] != 'github':
-        response = {
-            'error': 'Bad parameters: missing or invalid service'
-        }
+    if "service" not in data or data["service"] != "github":
+        response = {"error": "Bad parameters: missing or invalid service"}
         return jsonify(response), 400
 
-    if 'token' not in data:
-        response = {
-            'error': 'Bad parameters: missing token'
-        }
+    if "token" not in data:
+        response = {"error": "Bad parameters: missing token"}
         return jsonify(response), 400
-    
-    if 'prompt' not in data or len(data['prompt']) == 0:
-        response = {
-            'error': 'Bad parameters: missing or invalid prompt'
-        }
+
+    if "prompt" not in data or len(data["prompt"]) == 0:
+        response = {"error": "Bad parameters: missing or invalid prompt"}
         return jsonify(response), 400
-    
-    code, error = process_user_prompt()
+
+    service = data["service"]
+    token = data["token"]
+    prompt = data["prompt"]
+    code, error = process_user_prompt(service, token, prompt)
     if error is not None:
-        response = {
-            'error': error
-        }
+        response = {"error": error}
         return jsonify(response), 500
 
     response = {
-        'code': code,
+        "code": code,
     }
-    
+
     return jsonify(response)
 
-if __name__ == '__main__':
-    debug = os.getenv('DEBUG')
-    port = os.getenv('HTTP_PORT')
+
+if __name__ == "__main__":
+    debug = os.getenv("DEBUG")
+    port = os.getenv("HTTP_PORT")
 
     app.run(debug=debug, port=port)
--- database/services.py
+++ database/services.py
@@ -1,40 +1,50 @@
 from database import db
 
+
 class Services(db.Model):
+    __tablename__ = "services"  # Explicitly define the table name
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
-    
     description = db.Column(db.String(), nullable=False)
 
+
 class ServiceCategories(db.Model):
+    __tablename__ = "service_categories"  # Explicitly define the table name
     id = db.Column(db.Integer, primary_key=True)
-    service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey("services.id"), nullable=False)
     category = db.Column(db.String(80), nullable=False)
 
+
 class ServiceAPIs(db.Model):
+    __tablename__ = "service_apis"  # Explicitly define the table name
     id = db.Column(db.Integer, primary_key=True)
-    service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey("services.id"), nullable=False)
     version = db.Column(db.String(80), nullable=False)
     base_url = db.Column(db.String(120), nullable=False)
 
+
 class APIEndpoints(db.Model):
+    __tablename__ = "api_endpoints"  # Explicitly define the table name
     id = db.Column(db.Integer, primary_key=True)
-    service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
-    api_id = db.Column(db.Integer, db.ForeignKey('service_api.id'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey("services.id"), nullable=False)
+    api_id = db.Column(db.Integer, db.ForeignKey("service_apis.id"), nullable=False)
     path = db.Column(db.String(), nullable=False)
     method = db.Column(db.String(10), nullable=False)
     summary = db.Column(db.String(), nullable=True)
     description = db.Column(db.String(), nullable=True)
 
+
 class APIParameters(db.Model):
+    __tablename__ = "api_parameters"  # Explicitly define the table name
     id = db.Column(db.Integer, primary_key=True)
-    service_id = db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
-    endpoint_id = db.Column(db.Integer, db.ForeignKey('api_endpoint.id'), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey("services.id"), nullable=False)
+    endpoint_id = db.Column(
+        db.Integer, db.ForeignKey("api_endpoints.id"), nullable=False
+    )
     name = db.Column(db.String(), nullable=False)
     type = db.Column(db.String(), nullable=False)
     description = db.Column(db.String(), nullable=False)
     required = db.Column(db.Boolean, nullable=False)
-
 
 
 def DeleteService(session, service_name):
@@ -44,16 +54,22 @@ def DeleteService(session, service_name):
     # If the service exists, delete all related rows
     if service is not None:
         # Delete related rows in APIParameter
-        session.query(APIParameters).filter(APIParameters.service_id == service.id).delete()
+        session.query(APIParameters).filter(
+            APIParameters.service_id == service.id
+        ).delete()
 
         # Delete related rows in APIEndpoint
-        session.query(APIEndpoints).filter(APIEndpoints.service_id == service.id).delete()
+        session.query(APIEndpoints).filter(
+            APIEndpoints.service_id == service.id
+        ).delete()
 
         # Delete related rows in ServiceAPI
         session.query(ServiceAPIs).filter(ServiceAPIs.service_id == service.id).delete()
 
         # Delete related rows in ServiceCategory
-        session.query(ServiceCategories).filter(ServiceCategories.service_id == service.id).delete()
+        session.query(ServiceCategories).filter(
+            ServiceCategories.service_id == service.id
+        ).delete()
 
         # Finally, delete the service itself
         session.delete(service)
